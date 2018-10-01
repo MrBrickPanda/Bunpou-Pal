92edc5ea0bf82e14cc180f9dbbb2c637ef97137e
Asher Perkins <asherperkins11@gmail.com>
2018-10-01 22:33:27 +1000

    Create tokenise.py
    Parses through a sentence via MeCab and splits it up into word

---
+++ tokenise.py
@@ -0,0 +1,30 @@
+import MeCab
+from search_algorithm import *
+from natto import MeCab
+
+# Tokenise the sentense into morphemes using MeCabs parse feature, and adds the result to a tokenised
+def getToken(sentence):
+    tokenised = []
+    with MeCab(r'-F%m\n') as nm:
+        for n in nm.parse(sentence, as_nodes=True):
+            tokenised.append(n.feature)
+    return tokenised
+
+# This function assumes that the longest match in a string is the correct word.
+def parseSentence(sentence):
+    tokenised = getToken(sentence)
+    for token in tokenised:
+        tokenIter = tokenised
+        # Combines all the morphemes and searches JMdict for the combination
+        for tokenIndex in range(len(tokenised), 0, -1):
+            searchToken = ""
+            for item in tokenIter[0:tokenIndex]:
+                searchToken += item
+            wordDef = wordResults(searchToken)
+            # If a match is found, the morphemes used in the combination are removed from tokenised.
+            # This continues untill there are no morphemes left in tokenised.
+            if wordDef:
+                print(searchToken)
+                for i in tokenIter[0:tokenIndex]:
+                    tokenised.pop(0)
+                print(wordDef)
