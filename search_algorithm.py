e7fcf4787389994ded4d1a8f555731583bbc2136
Asher Perkins <asherperkins11@gmail.com>
2018-10-20 01:57:00 +1100

    General Updates
    Added functionality to search for English words

--- search_algorithm.py
+++ search_algorithm.py
@@ -12,7 +12,7 @@ class Search:
         self.keb = False
         self.rebs = ''
         # Connections to the dictionary database
-        self.conn = sqlite3.connect('jmd26.db')
+        self.conn = sqlite3.connect('jmd3.db')
         self.c = self.conn.cursor()
 
     def getSense(self, word):
@@ -39,8 +39,37 @@ class Search:
                         num += 1
                     wordStuff[-1] = wordStuff[-1].rstrip(",")+'. '
                     wordParts.append(wordStuff)
-
+# 事
         return wordParts
+        self.conn.commit()
+        self.c.close()
+        self.conn.close()
+
+    def getEnglish(self, word):
+        wordParts = []
+        tags = ['sense_id', 'xref', 'Meaning', 'Parts of Speech', 'miscellaneous', 'field', 's_inf', 'Dialect']
+        self.c.execute(f"SELECT * FROM Info WHERE gloss LIKE '% {word}%'")
+        for i in self.c.fetchall():
+            wordStuff = []
+            num = 0
+            for x in i:
+                wordD = tags[num] +': ' + str(x)
+                if wordD[-6:] != ': None':
+                    if 'sense_id' in wordD:
+                        self.c.execute(f"SELECT ent_seq FROM Sense WHERE Sense.sense_id = {wordD[10:]}")
+                        ent_seq = self.c.fetchone()
+                        self.c.execute(f"SELECT reb FROM Reading_element WHERE Reading_element.ent_seq = {ent_seq[0]}")
+                        hira = self.c.fetchone()
+                        self.c.execute(f"SELECT keb FROM Kanji_element WHERE Kanji_element.ent_seq = {ent_seq[0]}")
+                        kanji = self.c.fetchone()
+                        wordStuff.append(hira)
+                        wordStuff.append(kanji)
+                    else:
+                        wordStuff.append(wordD)
+                num += 1
+            wordStuff[-1] = wordStuff[-1].rstrip(",")+'. '
+            wordParts.append(wordStuff)
+        return wordParts       
         self.conn.commit()
         self.c.close()
         self.conn.close()
@@ -48,17 +77,19 @@ class Search:
 # Returns all relevent information of a word.
 def wordResults(word):
     finder = Search()
-    wData = finder.getSense(word)
-    if wData == []:
-        wData = finder.getSense(stemming(word))
-    # 事
-    return wData
+    if re.search(u'[\u0041-\u007a]', word):
+        return finder.getEnglish(word)
+    else:
+        wData = finder.getSense(word)
+        if wData == []:
+            wData = finder.getSense(stemming(word))
+        return wData
 
 def getKanji():
     rads = []
     conn = sqlite3.connect('rads.db')
     c = conn.cursor()
-    c.execute(f"SELECT * FROM Strokes")
+    c.execute("SELECT * FROM Strokes")
     results = c.fetchall()
     for i in results:
         rads.append(i)
@@ -66,3 +97,13 @@ def getKanji():
     c.close()
     conn.close()
     return rads
+
+def getRads(kanji):
+    radSet = set()
+    conn = sqlite3.connect('rads.db')
+    c = conn.cursor()
+    c.execute(f"SELECT * FROM {kanji}")
+    for i in c.fetchall():
+        for x in i[1]:
+            radSet.add(x)
+    return radSet
